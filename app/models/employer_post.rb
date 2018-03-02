8f4e1d9f6c29ec6648705d6ce04280e12bf6bc89
kofian <al.abdimi@gmail.com>
2018-03-02 08:49:29 +0000

    job location geocode

--- app/models/employer_post.rb
+++ app/models/employer_post.rb
@@ -3,20 +3,17 @@ class EmployerPost < ApplicationRecord
     has_one :insight
     has_one :job_location
     
-    validate :check_location?
-    #validates :card_number, presence: true, if: :paid_with_card?
-    #accepts_nested_attributes_for :insight
     accepts_nested_attributes_for :job_location
     #scope :eposts, -> (customer_id) { where(customer_id: customer_id) }
     scope :eposts, lambda { |customer| where(customer_id: customer)}
     scope :most_recent_first, lambda { order("updated_at DESC")}
     
     def check_location?
-        if EmployerPost.job_location == null
+        unless EmployerPost.job_location
         create_location(self.customer.address,self.customer.city,self.customer.state)
         end
     end    
     def create_location(address,city,state)
-        EmployerPost.job_location.create(location: address, city: city, state: state)
+        JobLocation.create(location: address, city: city, state: state)
     end
 end
